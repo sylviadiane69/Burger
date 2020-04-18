4aac4455d8453f4c8464966c486847e2abeee452
Sylvia Soto <sylviadiane69@gmail.com>
2020-04-17 22:14:12 -0500

    updated the connection js file

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('The Classic');
+INSERT INTO burgers (burger_name) VALUES ('The Dave Grohl');
+INSERT INTO burgers (burger_name) VALUES ('The Jaws')
+INSERT INTO burgers (burger_name) VALUES ('The Hot Stuff')
