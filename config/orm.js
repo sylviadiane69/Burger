ab9574063032ed2337bd539e1fd1adcd8e28642d
Sylvia Soto <sylviadiane69@gmail.com>
2020-04-19 17:33:44 -0500

    updated the orm.js file and the burger.js file

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+var connection = require("./connection.js");
+
+var orm = {
+  selectAll: function(whatToSelect, tableInput) {
+    var queryString = "SELECT ?? FROM ??";
+    connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  insertOne: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+    console.log(queryString);
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  updateOne: function(whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol) {
+    var queryString = "SELECT ?? FROM ?? AS tOne";
+    queryString += " LEFT JOIN ?? AS tTwo";
+    queryString += " ON tOne.?? = tTwo.??";
+
+    console.log(queryString);
+
+    connection.query(queryString, [whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol], function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
+};
+
+module.exports = orm;
