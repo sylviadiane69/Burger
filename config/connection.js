8371557096f853b170893e831d745e7f86ee4982
Sylvia Soto <sylviadiane69@gmail.com>
2020-04-27 18:37:22 -0500

    change to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,20 @@
 // Set up MySQL connection.
+var connection;
 var mysql = require("mysql");
+
   if (process.env.JAWSDB_URL) {
-    mysql.createConnection(process.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 
   } else {
 
-var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "ghm766",
   database: "burgers_db"
 });
-
+  }
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -21,7 +23,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-  }
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
